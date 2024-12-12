0e1990f9d46f12e1dcdcec778e5005c59ccce9f6
spanzy teo <spanzytheo@gmail.com>
2024-12-12 21:11:32 +0100

    recent footer modification

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,13 +6,13 @@ import { FaXTwitter } from 'react-icons/fa6'
 const Footer = () => {
   return (
     <div className="block">
-      <div className="h-[80px] bg-blue-700 lg:mt-[1100px] xl:mt-[990px] flex items-center justify-between px-16">
+      <div className="h-[80px] bg-[#0152FF] lg:mt-[1100px] xl:mt-[990px] flex items-center justify-between lg:px-16 px-4">
         <div>
-          <h1 className="text-white text-lg">© 2024 creation by</h1>
+          <h1 className="text-white lg:text-lg text-sm">© 2024 creation by</h1>
         </div>
         <div className="flex gap-4">
-          <FaXTwitter className="text-white h-[40px] w-[50px]" />
-          <FaTelegramPlane className="text-white h-[40px] w-[50px]" />
+          <FaXTwitter className="text-white lg:h-[40px] h-[30px] lg:w-[40px] w-[30px]" />
+          <FaTelegramPlane className="text-white lg:h-[40px] h-[30px] lg:w-[40px] w-[30px]" />
         </div>
       </div>
     </div>
